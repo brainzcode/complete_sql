053b459130042fe99994d793de3816df205d1520
brainzcode <98150590+brainzcode@users.noreply.github.com>
2024-11-19 10:51:41 -0700

    like sql command

---
+++ like.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM 
+    freelancers
+WHERE country LIKE '__e%'
+
+SELECT *
+FROM 
+    freelancers
+WHERE country LIKE '%es%'
+
+SELECT *
+FROM 
+    freelancers
+WHERE country LIKE '%es'
+
+SELECT *
+FROM 
+    freelancers
+WHERE country LIKE 'un%'
